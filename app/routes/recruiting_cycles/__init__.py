f927e25c609bea07164153bfc50b34e7de8935d1
Dan Schlosser <dan.r.schlosser@gmail.com>
2015-11-29 18:53:36 -0500

    styling and example secrets

--- app/routes/recruiting_cycles/__init__.py
+++ app/routes/recruiting_cycles/__init__.py
@@ -1,6 +1,4 @@
-import json
-
-from flask import Blueprint, request, redirect, render_template, session
+from flask import Blueprint, request, render_template
 
 from app.database import interviewees as interviewees_db
 from app.database import interviewers as interviewers_db
@@ -20,13 +18,20 @@ def get_time_slots(start_time, end_time):
 
     cur_time = start_time
     while cur_time < end_time:
+        display_time = cur_time % 12 if cur_time >= 13 else cur_time
+        am_pm = 'am' if cur_time < 12 else 'pm'
         if cur_time % 1 != 0:
             time_slot = "{cur_time}:30-{cur_time_plus_one}:00".format(
-                    cur_time=str(int(cur_time)), cur_time_plus_one=str(int(cur_time) + 1))
+                cur_time=str(int(cur_time)),
+                cur_time_plus_one=str(int(cur_time) + 1))
+            label = ''
         else:
-            time_slot = "{cur_time}:00-{cur_time}:30".format(cur_time=str(int(cur_time)))
+            time_slot = "{cur_time}:00-{cur_time}:30".format(
+                cur_time=str(int(cur_time)))
+            label = '{} {}'.format(str(int(display_time)), am_pm)
 
-        time_slots.append(time_slot)
+        print (time_slot, label)
+        time_slots.append((time_slot, label))
         cur_time += .5
 
     return time_slots
@@ -37,14 +42,15 @@ def get_interviewee_form(rc_id):
     if request.method == 'GET':
         days = ['12/5', '12/6', '12/7', '12/8', '12/9', '12/10']
         time_slots = get_time_slots(9, 23)
-        return render_template('interviewee_form.html', 
-                                days=days, time_slots=time_slots)
-    else: # POST
+        return render_template('interviewee_form.html',
+                               days=days, time_slots=time_slots)
+    else:  # POST
         name_and_availabilities = request.get_json()
         name = name_and_availabilities['name']
         availabilities = name_and_availabilities['availabilities']
 
-        interviewees_db.create_interviewee(name=name, availabilities=availabilities)
+        interviewees_db.create_interviewee(name=name,
+                                           availabilities=availabilities)
         return 'success'
 
 
@@ -53,12 +59,13 @@ def get_interviewer_form(rc_id):
     if request.method == 'GET':
         days = ['12/5', '12/6', '12/7', '12/8', '12/9', '12/10']
         time_slots = get_time_slots(9, 23)
-        return render_template('interviewer_form.html', 
-                                days=days, time_slots=time_slots)
-    else: # POST
+        return render_template('interviewer_form.html',
+                               days=days, time_slots=time_slots)
+    else:  # POST
         name_and_availabilities = request.get_json()
         name = name_and_availabilities['name']
         availabilities = name_and_availabilities['availabilities']
 
-        interviewers_db.create_interviewer(name=name, availabilities=availabilities)
+        interviewers_db.create_interviewer(name=name,
+                                           availabilities=availabilities)
         return 'success'
---
+++ app/templates/README.md
@@ -0,0 +1,18 @@
+When2Interview
+==============
+
+Developing
+----------
+
+1. Copy `config/example.secrets.py` into a new file called `secrets.py`, and set those secrets values using Google OAuth configurations.
+2. Run these commands:
+    ```
+    vagrant up
+    vagrant ssh
+    cd /vagrant
+    python run.py
+    ```
+3. Open `localhost:5000` to see the app!
+
+
+
