c0704155cfcb3c463f89a7d1a8ff6c764eaeabc8
Brian Zeng <thebrianzeng@gmail.com>
2015-11-29 11:51:23 -0500

    fix matching algorithm

--- app/routes/admin/__init__.py
+++ app/routes/admin/__init__.py
@@ -1,3 +1,6 @@
+import copy
+from pprint import pprint
+
 from flask import Blueprint, request, redirect, render_template, session
 
 admin = Blueprint('admin', __name__)
@@ -14,40 +17,46 @@ def create_recruiting_cycle():
     new_recruiting_cycle_url = '/recruiting_cycles/' + new_recruiting_cycle_id
     return redirect(new_recruiting_cycle_url)
 
-
-def match(interviewers_to_slots, interviewees_to_slots, current_schedule, possible_schedules=[]):
-    """ Generate the interview matching """
+def match(slots_to_interviewers, interviewees_to_slots, current_schedule, possible_schedules):
     if not interviewees_to_slots:
         possible_schedules.append(current_schedule)
+        return
 
-    for interviewer, interviewer_slots in interviewers_to_slots.items():
-        for interviewee, interviewee_slots in interviewees_to_slots.items():
-            for slot in interviewee_slots:
-                if slot in interviewer_slots:
-                    interviewer_interviewee_pair = {'interviewer': interviewer, 
+    for interviewee, interviewee_slots in interviewees_to_slots.viewitems():
+        for slot, interviewers in slots_to_interviewers.viewitems():
+            if slot in interviewee_slots:
+                for interviewer in interviewers:
+                    interviewer_interviewee_pair = {'interviewer': interviewer,
                                                     'interviewee': interviewee}
 
-                    if slot not in current_schedule:
-                        current_schedule[slot] = []
+                    new_current_schedule = copy.deepcopy(current_schedule)
 
-                    current_schedule[slot].append(interviewer_interviewee_pair)
-                    
-                    interviewees_to_slots.pop(interviewee)
-                    interviewers_to_slots[interviewer].remove(slot)
+                    if slot not in new_current_schedule:
+                        new_current_schedule[slot] = []
 
-                    if not interviewers_to_slots[interviewer]:
-                        interviewers_to_slots.pop(interviewer)
+                    new_current_schedule[slot].append(interviewer_interviewee_pair)
 
-                    current_schedule = match(interviewers_to_slots, interviewees_to_slots, current_schedule)
-                    
-    return possible_schedules # couldn't match them
+                    new_interviewees_to_slots = copy.deepcopy(interviewees_to_slots)
+                    new_interviewees_to_slots.pop(interviewee)
 
+                    new_slots_to_interviewers = copy.deepcopy(slots_to_interviewers)
+                    new_slots_to_interviewers[slot].remove(interviewer)
+
+                    match(new_slots_to_interviewers, new_interviewees_to_slots, new_current_schedule, possible_schedules)
+
+    return possible_schedules
+           
 
 if __name__ == '__main__':
     # Test case - normal
-    interviewers_to_slots = {'interviewer 1': ['slot1', 'slot2'],
-                             'interviewer 2': ['slot2', 'slot3']}
-    interviewees_to_slots = {'interviewee 1': ['slot1', 'slot5'],
-                             'interviewee 2': ['slot2', 'slot3']}
+    slots_to_interviewers = {'slot1': ['interviewer 1'],
+                             'slot2': ['interviewer 2']}
+                            # 'slot2': ['interviewer 1', 'interviewer 2']}
+#    interviewers_to_slots = {'interviewer 1': ['slot1', 'slot2'],
+#                             'interviewer 2': ['slot1', 'slot2']}
+    interviewees_to_slots = {'interviewee 1': ['slot1', 'slot2'],
+                             'interviewee 2': ['slot1', 'slot2']}
 
-    print match(interviewers_to_slots, interviewees_to_slots, {})
+    result = match(slots_to_interviewers, interviewees_to_slots, {}, [])
+    print '\nFinal result: '
+    pprint(result)
