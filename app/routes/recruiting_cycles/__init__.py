84adeae8618b2e2c9438bd399a05518869d5b588
Brian Zeng <thebrianzeng@gmail.com>
2015-11-29 15:08:57 -0500

    add transferring of availabilities from FE to BE

--- app/routes/recruiting_cycles/__init__.py
+++ app/routes/recruiting_cycles/__init__.py
@@ -14,10 +14,31 @@ def get_rc_page(rc_id):
     return render_template('recruiting_cycle.html')
 
 
+def get_time_slots(start_time, end_time):
+    """ Both times are hours (e.g. start_time=9 means 9am) """
+    time_slots = []
+
+    cur_time = start_time
+    while cur_time < end_time:
+        if cur_time % 1 != 0:
+            time_slot = "{cur_time}:30-{cur_time_plus_one}:00".format(
+                    cur_time=str(int(cur_time)), cur_time_plus_one=str(int(cur_time) + 1))
+        else:
+            time_slot = "{cur_time}:00-{cur_time}:30".format(cur_time=str(int(cur_time)))
+
+        time_slots.append(time_slot)
+        cur_time += .5
+
+    return time_slots
+
+
 @rc.route('/<rc_id>/interviewee_form', methods=['GET', 'POST'])
 def get_interviewee_form(rc_id):
     if request.method == 'GET':
-        return render_template('interviewee_form.html')
+        days = ['12/5', '12/6', '12/7', '12/8', '12/9', '12/10']
+        time_slots = get_time_slots(9, 23)
+        return render_template('interviewee_form.html', 
+                                days=days, time_slots=time_slots)
     else: # POST
         availabilities = request.get_json()
         return 'success'
