3fe11e14342c93891e21bbe6f2f37f767fd17261
Brian Zeng <thebrianzeng@gmail.com>
2015-11-30 18:36:17 -0500

    update deployment config

--- run.sh
+++ run.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 sudo docker rm interview_scheduler_instance
-sudo docker run --name interview_scheduler_instance --net=host -p 6003:6003 -i -t interview_scheduler_img
+sudo docker run \
+    --name interview_scheduler_instance \
+    --net=host \
+    -p 6003:6003 \
+    -v /srv/when2interview/logs:/opt/logs \
+    -d \
+    -i -t interview_scheduler_img
