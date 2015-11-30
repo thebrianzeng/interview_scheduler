ea71571778cee99e285d1e1320d00b77577f411d
Brian Zeng <thebrianzeng@gmail.com>
2015-11-30 18:15:25 -0500

    add deployment script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo docker rm interview_scheduler_instance
-sudo docker run --name interview_scheduler_instance -p 5000:5000 -i -t interview_scheduler_img
+sudo docker run --name interview_scheduler_instance --net=host -p 5000:5000 -i -t interview_scheduler_img
